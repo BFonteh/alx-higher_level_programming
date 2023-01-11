2a9cd9a8830aae3705f465160e65ed69f27ed2d5
B Fonteh <kiehfonteh@gmail.com>
2023-01-11 15:48:02 -0800

    create all files

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module 2-append_write"""
+
+
+def append_write(filename="", text=""):
+    """Function that appends a string to a text file
+    Returns:
+        number of characters written
+    """
+    with open(filename, mode='a', encoding="utf-8") as f:
+        f.write(text)
+
+    return len(text)
